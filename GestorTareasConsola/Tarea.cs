8d9d1022f09696e6efe8bb049f24cb9a92105d4a
wbeimar <wmarmonsa@gmail.com>
2025-07-29 13:47:41 -0500

    Agregando mejoras al codigo

--- GestorTareasConsola/Tarea.cs
+++ GestorTareasConsola/Tarea.cs
@@ -4,16 +4,16 @@ namespace GestorTareasConsola.Clases
 {
     public class Tarea
     {
-        public string Realizar { get; set; }     // Atributos o propiedades de una tarea
+        public string Realizar { get; set; }     // Lo que se debe realizar
         public DateTime? ParaQueDia { get; set; }  // Puede ser nula (opcional)
-        public bool Realizada { get; set; }
+        public bool Realizada { get; set; }    //Dice si ya esta completado
 
         // Constructor que nos ayuda a crear una nueva tarea
         public Tarea(string realizar, DateTime? paraQueDia = null)
         {
             Realizar = realizar;
             ParaQueDia = paraQueDia;
-            Realizada = false; // Por defecto, la tarea no está completada
+            Realizada = false; // Por defecto, la tarea se crea como pendiente
         }
 
         // Método que muestra la información de la tarea como texto
